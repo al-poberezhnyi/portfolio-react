f6e78e16431eba5ab23ffba97a1b0ec6fc48a3b6
al-poberezhnyi <al.poberezhnyi@gmail.com>
2025-02-19 12:05:08 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from 'react-router';
+import Layout from './pages/Layout';
+import Home from './pages/home';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import Projects from './pages/Projects';
+import NotFaund from './components/NotFaund';
+
 function App () {
     return (
         <>
-            <div></div>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/contact' element={<Contacts />} />
+                    <Route path='/project' element={<Projects />} />
+                    <Route path='/*' element={<NotFaund />} />
+                </Route>
+            </Routes>
         </>
     );
 }
